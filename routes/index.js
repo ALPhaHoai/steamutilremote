8679fed137ed5dcab72c13ba09a4c2b34d1fcc40
Natri <alphahoai@gmail.com>
2024-01-27 04:48:15 +0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,8 @@ router.post('/', async function (req, res, next) {
   try {
     const { method, params, cookies } = req.body
     const user = new Steamutils(cookies)
-    const fn = typeof user[method] === 'function' ? user[method] : Steamutils[method]
-    const result = await fn(...(params || []))
+    const self = typeof user[method] === 'function' ? user : Steamutils
+    const result = await self[method].apply(self, params)
     res.json({ result })
   } catch (e) {
     console.error(e)
