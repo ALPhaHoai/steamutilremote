58444b5230662300fdc4d8006c8fb0720e1bf0a5
Drake <drake@eup.net.vn>
2025-05-07 11:19:42 +0700

    Update dependencies: express and steamutils

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,23 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('steamutilremote Hello World')
+    res.send('steamutilremote Hello World')
 })
-router.post('/*', async function (req, res, next) {
-  try {
-    const method = req.url.replace(/^\//, "").trim() || req.body.method;
-    const { params, cookies, is_static } = req.body
-    const user = is_static ? Steamutils :  new Steamutils(cookies)
-    const result = await user[method].apply(user, params)
-    if (result instanceof ResponseError) {
-      return res.json({ error: true, message: result.message })
+router.post('/:method', async function (req, res, next) {
+    try {
+        const body = req.body || {};
+        const method = req.url.replace(/^\//, "").trim() || body.method;
+        const {params, cookies, is_static} = body
+        const user = is_static ? Steamutils : new Steamutils(cookies)
+        const result = await user[method].apply(user, params)
+        if (result instanceof ResponseError) {
+            return res.json({error: true, message: result.message})
+        }
+        res.json({result})
+    } catch (e) {
+        console.error(e)
+        res.json({error: true, message: e.message})
     }
-    res.json({ result })
-  } catch (e) {
-    console.error(e)
-    res.json({ error: true, message: e.message })
-  }
 })
 
 export default router
